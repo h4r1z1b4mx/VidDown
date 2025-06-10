9cb77265e93c80ddb5e8f6272bc6527ee435160c
h4r1z1b4mx <h4r1z1b4mx@gmail.com>
2025-06-10 14:08:20 +0530

    initialized

---
+++ backend/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.status(200).json({
+        msg: 'healthy'
+    });
+});
+app.listen(3000, () => {
+    console.log('Server is running on port ', 3000);
+});
